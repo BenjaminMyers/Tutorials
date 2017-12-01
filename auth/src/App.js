2be15da120a2730f9df95dbae92685fe659e22d2
Ben Myers <ben.myers@appian.com>
2017-12-01 14:56:17 -0500

    More Tutorial Stuff

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,15 +1,29 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import { View } from 'react-native';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBJbwpwBSlPl9l345FxB6KycV3ltNUvOpw',
+      authDomain: 'authentication-c5bca.firebaseapp.com',
+      databaseURL: 'https://authentication-c5bca.firebaseio.com',
+      projectId: 'authentication-c5bca',
+      storageBucket: 'authentication-c5bca.appspot.com',
+      messagingSenderId: '1026432059439'
+    });
+  }
+
   render() {
     return (
       <View>
           <Header headerText="Authentication" />
-          <Text> An App!</Text>
+          <LoginForm />
       </View>
-    )
+    );
   }
 }
 
